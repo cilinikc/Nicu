8b1067b652ef075c93f58dc2f9eed54eb6a04b98
cilinikc <88900599+cilinikc@users.noreply.github.com>
2021-08-24 16:22:14 +0300

    Update joc20210730-100023.cpp

--- joc20210730-100023.cpp
+++ joc20210730-100023.cpp
@@ -1,24 +1,24 @@
 #include<iostream>
+
 using namespace std;
 
-int main()
-{
-int i, s, u, x;
-s=time(0);
-srand(s);
-i=rand()%100+1;
-u=1;
-do {
+int main() {
+  int generate, sec, contour, inputx;
+  sec = time(0);
+  srand(sec);
+  generate = rand() % 100 + 1;
+  contour = 1;
+  do {
     cout << "Ghicește numarul ";
-    cin >>x;
-    if (i<x) 
-    cout <<"Prea mare. Mai încearcă "<<endl;
-    else if (i>x) 
-    cout <<"Prea mic. Mai încearcă " <<endl; 
-    else 
-    cout <<"Felicitări ai ghicit din " <<u<<" încercări";
-   u++;
-     }
-    while (i!=x) ;
-    return 0;
+    cin >> inputx;
+    if (generate < inputx)
+      cout << "Prea mare. Mai încearcă " << endl;
+    else if (generate > inputx)
+      cout << "Prea mic. Mai încearcă " << endl;
+    else
+      cout << "Felicitări ai ghicit din " << contour << " încercări";
+    contour++;
+  }
+  while (generate != inputx);
+  return 0;
 }
