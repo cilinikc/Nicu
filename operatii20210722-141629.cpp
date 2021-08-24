ff8890f270427c854620e83915fdb7b9195f8385
cilinikc <88900599+cilinikc@users.noreply.github.com>
2021-08-24 17:48:57 +0300

    Update operatii20210722-141629.cpp

--- operatii20210722-141629.cpp
+++ operatii20210722-141629.cpp
@@ -1,96 +1,90 @@
 #include<iostream>
 #include <cstdlib>
 #include <string>
-#include <sstream>
+#include <fstream>
 #include <iomanip>
 using namespace std;
-
 int main()
 {
-FILE *fisier ;
-int a, b, s, d, i, r, j, k, n, ts;
-string c, t,f,st;
-ostringstream sa; 
+ofstream file1 ;
+int number1, number2, result, result1,resultin, j, upto, many, sec;
+string operation,contin,st;
 time_t now=time(0);
-char *dt=ctime(&now) ;
-ts=time(0);
-srand(ts) ;
-f="d" ;
-while (f=="d") {
-c="d" ;
-    cout << "adunare=a scadere=s\n înmulțire=i împărțire=d ";
-    cin >>t;
-    cout <<"câte operații? " ;
-    cin >>n;
-    cout <<"operatii pana la cat? " ;
-    cin >>k;
-    for (j=1;j<=n;j++)
+char *data1=ctime(&now) ;
+sec=time(0);
+srand(sec) ;
+contin="y" ;
+while (contin=="y") {
+    cout << "addition=a subtraction=s\n multiplication=m division=d ";
+    cin >>operation;
+    cout <<"How many operations? " ;
+    cin >>many;
+    cout <<"Operations up to? " ;
+    cin >>upto;
+    file1.open("/storage/emulated/0/TextEditor/operatii.txt ") ;
+    for (j=1;j<=many;j++)
     { 
     
-    a=rand() %k+1;
-    b=rand()%k+1;
-    fg:
-    sa.str("") ;
-    if (t=="a") {
-    cout <<setw(4)<<a<<endl<<"+"<<setw(3) <<b<<endl<<"=" ;
-    cin >>r;
-    s=a+b;
-    sa<<a<<"+"<<b <<"=" <<r;
-    st=sa.str() ;
+    number1=rand()%upto+1;
+    number2=rand()%upto+1;
+    jump1:
+    if (operation=="a") {
+    cout <<setw(4)<<number1<<endl<<"+"<<setw(3) <<number2<<endl<<"=" ;
+    cin >>resultin;
+    result=number1+number2;
+    file1<<number1<<"+"<<number2 <<"=" <<r;
+   
     }
-    if (t=="d") {
-    i=a*b;
-    cout <<i<<":" <<b<<"=" ;
-    cin >>r;
-    s=i/b;
-    sa<<i<<":"<<b <<"=" <<r;
-    st=sa.str() ;
+    if (operation=="d") {
+    result1=number1*number2;
+    cout <<result1<<":" <<number2<<"=" ;
+    cin >>resultin;
+    result=number1;
+    file1<<result1<<":"<<number2 <<"=" <<r;
+    
     }
-    if (t=="i") {
-    cout <<a<<"x" <<b<<"=" ;
-    cin >>r;
-    s=a*b;
-    sa<<a<<"x"<<b <<"=" <<r;
-    st=sa.str() ;
+    if (operation=="m") {
+    cout <<number1<<"x" <<number2<<"=" ;
+    cin >>resultin;
+    result=number1*number2;
+    file1<<number1<<"x"<<number2 <<"=" <<r;
+    
     }
-    if (t=="s") {
+    if (operation=="s") {
     if(a>b) 
     {
-    sa<<a<<"-"<<b <<"=" <<r;
-    st=sa.str() ;
-    cout<<setw(4) <<a<<endl<<"-"<<setw(3) <<b<<endl<<"=" ;
-    cin>>r;
+    file1<<number1<<"-"<<number2 <<"=" <<r;
+    cout<<setw(4) <<number1<<endl<<"-"<<setw(3) <<number2<<endl<<"=" ;
+    cin>>resultin;
     }
     else {
-    cout <<setw(4)<<b<<endl<<"-" <<setw(3)<<a<<endl<<"=" ;
-    cin >>r;
-    sa<<b<<"-"<<a<<"=" <<r;
-    st=sa.str() ;
+    cout <<setw(4)<<number2<<endl<<"-" <<setw(3)<<number1<<endl<<"=" ;
+    cin >>resultin;
+    file1<<number2<<"-"<<number1<<"=" <<r;
     }
-    s=abs(a-b) ;
+    result=abs(number1-number2) ;
     }
     if(cin. fail()) {
     std::cin. clear() ;
     std::cin. ignore() ;
-    goto fg;
+    goto jump1;
     }
-    fisier=fopen("/storage/emulated/0/TextEditor/operatii.txt ", "a") ;
-fprintf(fisier,"%s", st ) ;
-    if (r==s) {
-    fprintf(fisier,"ok") ;
+    
+    if (resultin==result) {
+    file1<<"ok" ;
     cout <<"ok\n" ;
     }
     else {
-    fprintf(fisier,"gresit trebuia %d", s) ;
-    cout <<"gresit trebuia " <<s<<endl;
+    file1<<"wrong it had to "<< result ;
+    cout <<"gresit trebuia " <<result<<endl;
     }
-    fclose(fisier) ;
+    file1.close();
     }
     cout <<"\nalta operatie? d/n " ;
-    cin >>f;
+    cin >>contin;
     }
-    fisier=fopen("/storage/emulated/0/TextEditor/operatii.txt ", "a") ;
-    fprintf(fisier,"%s ", dt ) ;
-    fclose(fisier) ;
+    file1.open("/storage/emulated/0/TextEditor/operatii.txt ") ;
+    file1<<data1 ;
+    file1.close() ;
     return 0;
 }
