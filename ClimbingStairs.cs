c4a8146a32a09ac8308661d0fa61f62bd63d9fba
cilinikc <88900599+cilinikc@users.noreply.github.com>
2021-10-24 18:27:49 +0300

    Update ClimbingStairs.cs
    You are climbing a staircase. It takes n steps to reach the top.
    Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?

--- ClimbingStairs.cs
+++ ClimbingStairs.cs
@@ -6,7 +6,7 @@ namespace letcode
     {
         static void Main()
         {
-            int n = 4;
+            int n = Convert.ToInt32(Console.ReadLine());
             int result = 0;
             int prevPrev = 1;
             int prev = 2;
@@ -15,12 +15,11 @@ namespace letcode
             if (n == 2) result = 2;
             for (int i = 3; i <= n; i++)
             {
-                result = prev + prevPrev;
+                result = prev + prevPrev;                  
                 prevPrev = prev;
-                prev = result;                
+                prev = result;              
             }
             Console.WriteLine( result);
-
         }
     }
 }
