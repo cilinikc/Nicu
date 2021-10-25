f40ad93317f8fd39eb370c9d5f9e3d1c4adc5508
cilinikc <88900599+cilinikc@users.noreply.github.com>
2021-10-25 19:45:18 +0300

    Add files via upload
    You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
    Increment the large integer by one and return the resulting array of digits.

---
+++ incrementArray.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace letcode
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] digits = { 8, 9, 9, 9 };
+            bool is9 = false;
+            int adds = 0;
+            int count = 0;
+            for (int i = 0; i < digits.Length; i++)
+            {
+                if (digits[i] == 9)
+                    is9 = true;
+                else
+                {
+                    is9 = false;
+                    break;
+                }
+            }
+            if (is9)
+            {
+                int[] result = new int[digits.Length + 1];
+                for (int i = 0; i < result.Length; i++)
+                    result[i] = 0;
+                result[0] = 1;
+                for (int i = 0; i < result.Length; i++)
+                    Console.Write("{0},", result[i]);
+            }
+            else
+            {
+                int[] result = new int[digits.Length];
+                for (int i = result.Length - 1; i >= 0; i--)
+                {
+
+                    if ((digits[i] == 9) && (digits.Length - i - 1 == count))
+                    {
+                        result[i] = 0;
+                        adds = 1;
+                        count += 1;
+                    }
+                    else if (i == result.Length - 1)
+                        result[i] = digits[i] + 1;
+                    else
+                    {
+                        result[i] = digits[i] + adds;
+                        adds = 0;
+                    }
+
+                }
+                for (int i=0;i<result.Length;i++)
+                Console.Write("{0},",result[i]);
+            }
+        
+        }
+    }
+}
+
