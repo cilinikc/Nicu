a8b036d0e398c3f3747f2a1840c64e06f4fdaf93
cilinikc <88900599+cilinikc@users.noreply.github.com>
2021-11-02 20:13:23 +0200

    Add files via upload
    Enter 3 numbers with space between, by 3 lines and 15 numbers extracted to see if you made line or bingo.

---
+++ Bingo.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Probe
+{
+    class Program
+    {
+        private static void Main()
+        {
+            int lineOk = 0;
+            const int line = 3;
+            const int line2 = 3;
+            const int line3 = 6;
+            const int entries = 9;
+            const int extract = 15;
+            int[] cart = new int[9];
+            int[] extracted = new int[15];
+            int count = 0;
+            string input = "";
+            for (int i = 0; i < line; i++)
+            {
+                input += Console.ReadLine() + ' ';
+            }
+
+            for (int i = 0; i < extract; i++)
+            {
+                extracted[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            string[] inputArray = input.Split(' ');
+            for (int i = 0; i < entries; i++)
+            {
+                cart[i] = Convert.ToInt32(inputArray[i]);
+            }
+
+            for (int i = 0; i < entries; i++)
+            {
+                if ((i == line2) || (i == line3))
+                {
+                    count = 0;
+                }
+
+                for (int k = 0; k < extract; k++)
+                {
+                     if (cart[i] == extracted[k])
+                     {
+                          count++;
+                     }
+                }
+
+                if (count == line)
+                {
+                    lineOk++;
+                }
+            }
+
+            Console.WriteLine(Print(lineOk));
+        }
+
+        static string Print(int lineOk)
+        {
+            const int line = 3;
+            if (lineOk == line)
+            {
+                return "bingo";
+            }
+            else if (lineOk > 0)
+            {
+                return "linie";
+            }
+            else
+            {
+                return "nimic";
+            }
+        }
+    }
+}
