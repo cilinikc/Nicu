7406a12f6dff9d35f2147f54eb7ebf5bea55dac6
cilinikc <88900599+cilinikc@users.noreply.github.com>
2021-11-02 20:10:44 +0200

    Add files via upload
    Enter the number of excel colum to see its name.

---
+++ nrExcelcolum.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Probe
+{
+    class Program
+    {
+        private static void Main()
+        {
+            const int asciiStart = 64;
+            const int setChar = 26;
+            int inputExcelCol = Convert.ToInt32(Console.ReadLine());
+            char[] leters = new char[10];
+            int firstNumber;
+            int secondNumber;
+            int i = 0;
+            firstNumber = inputExcelCol / setChar;
+            secondNumber = inputExcelCol % setChar;
+            if (inputExcelCol > setChar)
+            {
+                while (inputExcelCol > 0)
+                {
+                    if ((secondNumber == 0) && (firstNumber <= setChar) && (firstNumber >= 1) && (i > 0))
+                    {
+                        secondNumber = firstNumber;
+                    }
+                    else if (secondNumber == 0)
+                    {
+                        secondNumber = setChar;
+                        firstNumber--;
+                    }
+
+                    leters[i] = (char)(asciiStart + secondNumber);
+                    i++;
+                    inputExcelCol = firstNumber;
+                    firstNumber = inputExcelCol / setChar;
+                    secondNumber = inputExcelCol % setChar;
+                }
+            }
+            else
+            {
+                if (secondNumber == 0)
+                {
+                    secondNumber = setChar;
+                }
+
+                leters[i] = (char)(asciiStart + secondNumber);
+            }
+
+            Print(leters);
+        }
+
+        static void Print(char[] leters)
+        {
+            for (int i = leters.Length - 1; i >= 0; i--)
+            {
+                if (leters[i] != 0)
+                {
+                    Console.Write(leters[i]);
+                }
+            }
+        }
+    }
+}
